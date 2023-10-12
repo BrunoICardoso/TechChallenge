ca63f9cda8b46c6ce0e5f5e1dc215a7bf33a086c
Raffael Eloi <raffaeleloi121@gmail.com>
2023-10-12 12:25:14 -0300

    Add ProductController with routes

---
+++ BurgerRoyale/src/BurgerRoyale.API/Controllers/Product/ProductController.cs
@@ -0,0 +1,85 @@
+﻿using BurgerRoyale.API.ConfigController;
+using BurgerRoyale.API.Extensions;
+using BurgerRoyale.Application.DTO;
+using BurgerRoyale.Application.Interface.Services;
+using BurgerRoyale.Application.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BurgerRoyale.API.Controllers.Product
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : BaseController
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ProductResponse), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> Add([FromBody] ProductDTO productDTO)
+        {
+            ProductResponse response = await _productService.AddAsync(productDTO);
+
+            if (response.IsValid)
+            {
+                return StatusCode(StatusCodes.Status201Created, response);
+            }
+
+            return ValidationProblem(ModelState.AddErrosFromNofifications(response.Notifications));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(GetProductResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            GetProductResponse response = await _productService.GetByIdAsync(id);
+
+            if (response.IsValid)
+            {
+                return Ok(response);
+            }
+
+            return ValidationProblem(ModelState.AddErrosFromNofifications(response.Notifications));
+        }
+        
+        [HttpPut]
+        [ProducesResponseType(typeof(ProductResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> Update(Guid id, [FromBody] ProductDTO productDTO)
+        {
+            ProductResponse response = await _productService.UpdateAsync(id, productDTO);
+
+            if (response.IsValid)
+            {
+                return Ok(response);
+            }
+
+            return ValidationProblem(ModelState.AddErrosFromNofifications(response.Notifications));
+        }
+        
+        [HttpDelete]
+        [ProducesResponseType(typeof(ProductResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> Remove(Guid id)
+        {
+            ProductResponse response = await _productService.RemoveAsync(id);
+
+            if (response.IsValid)
+            {
+                return Ok(response);
+            }
+
+            return ValidationProblem(ModelState.AddErrosFromNofifications(response.Notifications));
+        }
+    }
+}
