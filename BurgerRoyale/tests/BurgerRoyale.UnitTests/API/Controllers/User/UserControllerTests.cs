836b71a7ec4dd3340f7ed42adc2ec4653b927f59
Douglas Fuelber <dev@douglasfuelber.com>
2023-10-25 22:55:22 -0300

    Added user controller test

---
+++ BurgerRoyale/tests/BurgerRoyale.UnitTests/API/Controllers/User/UserControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using BurgerRoyale.API.Controllers.User;
+using BurgerRoyale.Domain.DTO;
+using BurgerRoyale.Domain.Enumerators;
+using BurgerRoyale.Domain.Interface.Services;
+using BurgerRoyale.Domain.ResponseDefault;
+using BurgerRoyale.UnitTests.Domain.EntitiesMocks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Net;
+using Xunit;
+
+namespace BurgerRoyale.UnitTests.API.Controllers.User
+{
+    public class UserControllerTests
+    {
+        private readonly Mock<IUserService> _userService;
+
+        private readonly UserController _userController;
+
+        public UserControllerTests()
+        {
+            _userService = new Mock<IUserService>();
+
+            _userController = new UserController(_userService.Object);
+        }
+
+        [Fact]
+        public async Task GivenGetUsersRequest_WhenGetUsers_ThenShouldReturnList()
+        {
+            // arrange
+            _userService
+                .Setup(x => x.GetUsersAsync(It.IsAny<UserType>()))
+                .ReturnsAsync(new List<UserDTO>());
+
+            // act
+            var response = await _userController.GetUsers(UserType.Customer) as ObjectResult;
+
+            // assert
+            response.Should().NotBeNull();
+            response?.StatusCode.Should().Be((int)HttpStatusCode.OK);
+            response?.Value.Should().BeOfType<ReturnAPI<IEnumerable<UserDTO>>>();
+        }
+    }
+}
