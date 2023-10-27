ecf110eaadb293bae81d6c89a61eca4455e5a7dd
Raffael Eloi <raffaeleloi121@gmail.com>
2023-10-26 21:41:24 -0300

    Check the status of application (GREEN)

---
+++ BurgerRoyale/tests/BurgerRoyale.IntegrationTests/Features/HealthCheck.feature
@@ -0,0 +1,6 @@
+﻿Feature: HealthCheck
+
+Scenario: Check the status of application
+	Given I want to check the status of my application
+	When I check the application status
+	Then I should see the status as ok
---
+++ BurgerRoyale/tests/BurgerRoyale.IntegrationTests/StepDefinitions/HealthCheckStepDefinitions.cs
@@ -0,0 +1,37 @@
+using BurgerRoyale.IntegrationTests.Helpers;
+
+namespace BurgerRoyale.IntegrationTests.StepDefinitions
+{
+    [Binding]
+    public class HealthCheckStepDefinitions
+    {
+        private readonly ScenarioContext _scenarioContext;
+        
+        private readonly HttpClient _httpClient;
+
+        public HealthCheckStepDefinitions(ScenarioContext scenarioContext, HttpClientInject httpClientInject)
+        {
+            _scenarioContext = scenarioContext;
+            _httpClient = httpClientInject.GetProgram().CreateClient();
+        }
+
+        [Given(@"I want to check the status of my application")]
+        public void GivenIWantToCheckTheStatusOfMyApplication()
+        {
+        }
+
+        [When(@"I check the application status")]
+        public async Task WhenICheckTheApplicationStatus()
+        {
+            var httpResponse = await _httpClient.GetAsync($"{HttpClientRequest.Path}/health");
+            _scenarioContext["healthCheckHttpResponse"] = httpResponse;
+        }
+
+        [Then(@"I should see the status as ok")]
+        public void ThenIShouldSeeTheStatusAsOk()
+        {
+            var httpResponse = _scenarioContext.Get<HttpResponseMessage>("healthCheckHttpResponse");
+            httpResponse.EnsureSuccessStatusCode();
+        }
+    }
+}
