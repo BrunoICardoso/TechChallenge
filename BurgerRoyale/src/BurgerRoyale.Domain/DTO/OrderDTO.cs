224f959a342038c3fae6198a20a1cd14924020f2
Douglas Fuelber <dev@douglasfuelber.com>
2023-10-26 22:43:26 -0300

    Changes in order service

--- BurgerRoyale/src/BurgerRoyale.Domain/DTO/OrderDTO.cs
+++ BurgerRoyale/src/BurgerRoyale.Domain/DTO/OrderDTO.cs
@@ -1,4 +1,7 @@
-﻿namespace BurgerRoyale.Domain.DTO
+﻿using BurgerRoyale.Domain.Entities;
+using BurgerRoyale.Domain.Helpers;
+
+namespace BurgerRoyale.Domain.DTO
 {
 	public class OrderDTO
 	{
@@ -7,7 +10,21 @@
 		public string Status { get; set; }
 		public DateTime OrderTime { get; set; }
 		public DateTime? CloseTime { get; set; }
-		public decimal TotalPrice { get; set; }
+		public decimal TotalPrice {
+			get {
+				return OrderProducts.Sum(x => x.Price * x.Quantity);
+			}
+		}
 		public IEnumerable<OrderProductDTO> OrderProducts { get; set; } = Enumerable.Empty<OrderProductDTO>();
-	}
+
+        public OrderDTO(Order order)
+        {
+			OrderId = order.Id;
+			Status = order.Status.GetDescription();
+			UserId = order.UserId;
+			OrderTime = order.OrderTime;
+			CloseTime = order.CloseTime;
+			OrderProducts = order.OrderProducts.Select(orderProduct => new OrderProductDTO(orderProduct));
+        }
+    }
 }
--- BurgerRoyale/src/BurgerRoyale.Domain/Entities/Order.cs
+++ BurgerRoyale/src/BurgerRoyale.Domain/Entities/Order.cs
@@ -4,11 +4,28 @@ namespace BurgerRoyale.Domain.Entities
 {
 	public class Order : Entity
 	{
-		public Guid UserId { get; set; }
-		public DateTime OrderTime { get; set; }
-		public DateTime? CloseTime { get; set; }
-		public OrderStatus Status { get; set; }
+		public Guid UserId { get; private set; }
+		public DateTime OrderTime { get; private set; }
+		public DateTime? CloseTime { get; private set; }
+		public OrderStatus Status { get; private set; }
 
-		public virtual IEnumerable<OrderProduct> OrderProducts { get; set; }
-	}
+		public virtual List<OrderProduct> OrderProducts { get; private set; } = new List<OrderProduct>();
+
+        public Order(Guid userId)
+        {
+			UserId = userId;
+			OrderTime = DateTime.Now;
+			Status = OrderStatus.Recebido;
+        }
+
+		public void AddProduct(OrderProduct orderProduct)
+		{
+			OrderProducts.Add(orderProduct);
+		}
+
+		public void SetStatus(OrderStatus status)
+		{
+			Status = status;
+		}
+    }
 }
