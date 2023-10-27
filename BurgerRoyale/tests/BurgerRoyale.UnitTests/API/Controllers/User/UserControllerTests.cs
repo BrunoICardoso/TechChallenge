31dac415bbd77058983699f2c4e1947624e85c3e
Douglas Fuelber <dev@douglasfuelber.com>
2023-10-26 22:02:01 -0300

    User controller tests

--- BurgerRoyale/tests/BurgerRoyale.UnitTests/API/Controllers/User/UserControllerTests.cs
+++ BurgerRoyale/tests/BurgerRoyale.UnitTests/API/Controllers/User/UserControllerTests.cs
@@ -26,7 +26,7 @@ namespace BurgerRoyale.UnitTests.API.Controllers.User
         }
 
         [Fact]
-        public async Task GivenGetUsersRequest_WhenGetUsers_ThenShouldReturnList()
+        public async Task GivenGetUsersRequest_WhenGetUsers_ThenShouldReturnListWithStatusOk()
         {
             // arrange
             _userService
@@ -40,6 +40,105 @@ namespace BurgerRoyale.UnitTests.API.Controllers.User
             response.Should().NotBeNull();
             response?.StatusCode.Should().Be((int)HttpStatusCode.OK);
             response?.Value.Should().BeOfType<ReturnAPI<IEnumerable<UserDTO>>>();
+        }
+
+        [Fact]
+        public async Task GivenGetUserRequest_WhenGetUser_ThenShouldReturnUserWithStatusOk()
+        {
+            // arrange
+            var user = UserMock.Get();
+
+            _userService
+                .Setup(x => x.GetByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(new UserDTO(user));
+
+            // act
+            var response = await _userController.GetUser(user.Id) as ObjectResult;
+
+            // assert
+            response.Should().NotBeNull();
+            response?.StatusCode.Should().Be((int)HttpStatusCode.OK);
+            response?.Value.Should().BeOfType<ReturnAPI<UserDTO>>();
+
+            var data = ((ReturnAPI<UserDTO>?) response?.Value)?.Data;
+
+            data?.Id.Should().Be(user.Id);
+        }
+
+        [Fact]
+        public async Task GivenRequestUserDto_WhenCreateUser_ThenShouldReturnCreatedUserWithStatusCreated()
+        {
+            // arrange
+            var user = UserMock.Get();
+
+            var request = new RequestUserDTO()
+            {
+                Cpf = user.Cpf,
+                Email = user.Email,
+                Name = user.Name,
+                UserType = user.UserType
+            };
+
+            _userService
+                .Setup(x => x.CreateAsync(request))
+                .ReturnsAsync(new UserDTO(user));
+
+            // act
+            var response = await _userController.CreateUser(request) as ObjectResult;
+
+            // assert
+            response.Should().NotBeNull();
+            response?.StatusCode.Should().Be((int)HttpStatusCode.Created);
+            response?.Value.Should().BeOfType<ReturnAPI<UserDTO>>();
+
+            var data = ((ReturnAPI<UserDTO>?)response?.Value)?.Data;
+
+            data?.Email.Should().Be(user.Email);
+        }
+
+        [Fact]
+        public async Task GivenRequestUserDtoAndId_WhenUpdateUser_ThenShouldReturnUpdatedUserWithStatusOk()
+        {
+            // arrange
+            var user = UserMock.Get();
+
+            var request = new RequestUserDTO()
+            {
+                Cpf = user.Cpf,
+                Email = user.Email,
+                Name = user.Name,
+                UserType = user.UserType
+            };
+
+            _userService
+                .Setup(x => x.UpdateAsync(user.Id, request))
+                .ReturnsAsync(new UserDTO(user));
+
+            // act
+            var response = await _userController.UpdateUser(user.Id, request) as ObjectResult;
+
+            // assert
+            response.Should().NotBeNull();
+            response?.StatusCode.Should().Be((int)HttpStatusCode.OK);
+            response?.Value.Should().BeOfType<ReturnAPI<UserDTO>>();
+
+            var data = ((ReturnAPI<UserDTO>?)response?.Value)?.Data;
+
+            data?.Email.Should().Be(user.Email);
+        }
+
+        [Fact]
+        public async Task GivenUserId_WhenDeleteUser_ThenShouldReturnStatusNoContent()
+        {
+            // arrange
+            var userId = Guid.NewGuid();
+
+            // act
+            var response = await _userController.DeleteUser(userId) as ObjectResult;
+
+            // assert
+            response.Should().NotBeNull();
+            response?.StatusCode.Should().Be((int)HttpStatusCode.NoContent);
         }
     }
 }
